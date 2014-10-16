5b6ee5fecc60b6b51058ef71d5cd0d81f0d97c3c
drindking@gmail.com <drindking@gmail.com@367c6aed-77c1-c063-999c-c6c833199797>
2014-10-16 08:08:39 +0000

    * first step rebuild

--- trunk/source/3DRebuild/main.cpp
+++ trunk/source/3DRebuild/main.cpp
@@ -1,28 +1,90 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QElapsedTimer>
-//#include "Vector.h"
-//#include "Quaternion.h"
-//#include "Matrix.h"
+#include "Vector.h"
+#include "Quaternion.h"
+#include "Matrix.h"
 #include "opencv2/opencv.hpp"
+#include <QMap>
+#include <QList>
+
+struct PointCC
+{
+	QPoint leftDot;
+	QPoint rightDot;
+	CVector realDot;
+	unsigned char color[3];
+};
+QList<PointCC> finalPoints;
+
+
+
+bool Save(const char *filename)
+{
+	//wchar_t *p,*n;
+	//wchar_t tfile[256];
+	//unsigned char tmpbuff = 1;
+	//char boneId = 0;
+
+	FILE *fp = fopen(filename, "wb");
+	if (!fp)
+		return false;
+
+	fseek(fp, 0, SEEK_SET);
+
+	char id[11] = "DS3D000000";
+	fwrite(id, sizeof(char), 10, fp);
+
+	int version = 4;
+	fwrite(&version, sizeof(int), 1, fp);
+
+	// vertices
+	unsigned int numVertices = finalPoints.size();
+	fwrite(&numVertices, sizeof(unsigned int), 1, fp);
+	for(int i = 0; i < numVertices; i++)
+	{
+		float x = finalPoints[i].realDot.x;
+		float y = finalPoints[i].realDot.y;
+		float z = finalPoints[i].realDot.z;
+		unsigned char vcolor[3] = {255};
+		fwrite(&x, sizeof(float), 1, fp);
+		fwrite(&y, sizeof(float), 1, fp);
+		fwrite(&z, sizeof(float), 1, fp);
+		fwrite(finalPoints[i].color, sizeof(char), 3, fp);
+	}
+	// triangles
+	unsigned short numTriangles = 0;
+	fwrite(&numTriangles, sizeof(unsigned short), 1, fp);
+	// groups
+	unsigned short numGroups = 0;
+	fwrite(&numGroups, sizeof(unsigned short), 1, fp);
+
+	// materials
+	unsigned short numMaterials = 0;
+	fwrite(&numMaterials, sizeof(unsigned short), 1, fp);
+	fclose(fp);
+
+}
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-	//CVector vec;
-	//Quaternion qut;
-	//Matrix matPrj;
-	//matPrj.LoadIdentity();
-	//matPrj.Perspective(22.5, 1.333333333, 50.0, 2000.0);
-	//Matrix matLMv;
-	//matLMv.LoadIdentity();
-	//matLMv.Rotate(15, CVector(0.0f, 1.0f, 0.0f));
-	//matLMv.Translate(CVector(100, 0, 0));
-	//Matrix matRMv;
-	//matRMv.LoadIdentity();
-	//matRMv.Rotate(-7, CVector(0.0f, 1.0f, 0.0f));
-	//matRMv.Translate(CVector(-100, 0, 0));
+	CVector vec;
+	Quaternion qut;
+	Matrix matPrj;
+	matPrj.LoadIdentity();
+	matPrj.Perspective(22.5, 1.333333333, 50.0, 2000.0);
+	Matrix matLMv;
+	matLMv.LoadIdentity();
+	matLMv.Rotate(15, CVector(0.0f, 1.0f, 0.0f));
+	matLMv.Translate(CVector(100, 0, 0));
+	Matrix matRMv;
+	matRMv.LoadIdentity();
+	matRMv.Rotate(-7, CVector(0.0f, 1.0f, 0.0f));
+	matRMv.Translate(CVector(-100, 0, 0));
+	const int viewport[4] = {0, 0, 1280, 960};
+	CVector leftCameraPos(-100, 0, 0);
+	CVector rightCameraPos(100, 0, 0);
 	//CVector screenPoint(610, 951, 1.0);
-	//const int viewport[4] = {0, 0, 1280, 960};
 	//CVector vecF = Matrix::UnprojectPoint(screenPoint, matLMv, matPrj, viewport);
 
 
@@ -33,10 +95,14 @@ int main(int argc, char *argv[])
 	//CVector pdt, pdt2;
 	//CVector::GetShortestBridge(planePoint, planeNormal, linePoint, lineDir, pdt, pdt2);
 
+
 	IplImage *lAll = NULL;
 	IplImage *lDark = NULL;
 	IplImage *rAll = NULL;
 	IplImage *rDark = NULL;
+	IplImage *lBinMap = NULL;
+	IplImage *rBinMap = NULL;
+	IplImage *colorImg = NULL;
 	IplImage *lBars[12] = {NULL};
 	IplImage *rBars[12] = {NULL};
 	IplImage *lRBars[12] = {NULL};
@@ -56,6 +122,11 @@ int main(int argc, char *argv[])
 	lAll = cvLoadImage("ImageL14.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 	rDark = cvLoadImage("ImageR13.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 	rAll = cvLoadImage("ImageR14.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+	colorImg = cvLoadImage("ImageL14.bmp", CV_LOAD_IMAGE_COLOR);
+	lBinMap = cvCreateImage(cvGetSize(lAll), IPL_DEPTH_16U, 1);
+	rBinMap = cvCreateImage(cvGetSize(rAll), IPL_DEPTH_16U, 1);
+	cvSetZero(lBinMap);
+	cvSetZero(rBinMap);
 	for(int i = 12; i > 0; i--)
 	{
 		lBars[12 - i] = cvLoadImage(QString("ImageL%1.bmp").arg(i).toLatin1(), CV_LOAD_IMAGE_GRAYSCALE);
@@ -94,16 +165,176 @@ int main(int argc, char *argv[])
 		cvSub(lAll, lBars[i], lRBars[i]);
 		cvSub(rAll, rBars[i], rRBars[i]);
 	}
-	int cannyThreshold = 10;
-	for(int i = 0; i < 12; i++)
+
+	int threshold = 20;
+	CvSize lSize = cvGetSize(lAll);
+	CvSize rSize = cvGetSize(rAll);
+	QElapsedTimer etimer;
+	etimer.start();
+	unsigned int fac = 1280;
+	for(int idx = 0; idx < 8; idx++)
 	{
-		cvCanny(lBars[i], lCanny[i], cannyThreshold, cannyThreshold * 2, 3);
-		cvCanny(rBars[i], rCanny[i], cannyThreshold, cannyThreshold * 2, 3);
-		cvCanny(lRBars[i], lRCanny[i], cannyThreshold, cannyThreshold * 2, 3);
-		cvCanny(rRBars[i], rRCanny[i], cannyThreshold, cannyThreshold * 2, 3);
-		cvAnd(lCanny[i], lRCanny[i], lAndCanny[i]);
-		cvAnd(rCanny[i], rRCanny[i], rAndCanny[i]);
+		//CvScalar blackValue = cvScalarAll(0);
+		unsigned char barPixel;
+		unsigned char rBarPixel;
+		unsigned char allPix;
+		for(int i = 0; i < lSize.width * lSize.height; i++)
+		{
+			allPix = *(lAll->imageData + i);
+			if(allPix >0)
+			{
+				barPixel = *(lBars[idx]->imageData + i);
+				rBarPixel = *(lRBars[idx]->imageData + i);
+				if(barPixel > rBarPixel)
+				{
+					unsigned short *pData = (unsigned short*)(lBinMap->imageData);
+					pData += i;
+					*(pData) += fac;
+				}
+			}
+			allPix = *(rAll->imageData + i);
+			if(allPix > 0)
+			{
+				barPixel = *(rBars[idx]->imageData + i);
+				rBarPixel = *(rRBars[idx]->imageData + i);
+				if(barPixel > rBarPixel)
+				{
+					unsigned short *pData = (unsigned short*)(rBinMap->imageData);
+					pData += i;
+					*(pData) += fac;
+				}
+			}
+		}
+		fac /= 2;
 	}
+
+	for(int idx = 8; idx < 9; idx++)
+	{
+		//IplImage *lCalBin = cvCreateImage(cvGetSize(lAll), IPL_DEPTH_16U, 1);
+		//IplImage *rCalBin = cvCreateImage(cvGetSize(rAll), IPL_DEPTH_16U, 1);
+		//cvCopy(lBinMap, lCalBin);;
+		//cvCopy(rBinMap, rCalBin);;
+		unsigned char barPixel;
+		unsigned char rBarPixel;
+		unsigned char allPix;
+		//QMap< baridx, QMap<Y, QPair<QList<LeftX>, QList<RightX> > > >
+		QMap<quint32, QMap<quint32,  QPair<QList<quint32>, QList<quint32> > > > bars;
+		for(int i = 0; i < lSize.width * lSize.height; i++)
+		{
+			int y = i / lSize.width;
+			int x = i % lSize.width;
+			if(y < 3)
+			{
+				continue;
+			}
+			allPix = *(lAll->imageData + i);
+			if(allPix >0)
+			{
+				barPixel = *(lBars[idx]->imageData + i);
+				rBarPixel = *(lRBars[idx]->imageData + i);
+				unsigned short *pData = (unsigned short*)(lBinMap->imageData);
+				pData += i;
+				unsigned short val = *pData;
+				if(barPixel > rBarPixel)
+				{
+					val++;
+				}
+				bars[val][y].first.append(x);
+			}
+			allPix = *(rAll->imageData + i);
+			if(allPix > 0)
+			{
+				barPixel = *(rBars[idx]->imageData + i);
+				rBarPixel = *(rRBars[idx]->imageData + i);
+				unsigned short *pData = (unsigned short*)(rBinMap->imageData);
+				pData += i;
+				unsigned short val = *pData;
+				if(barPixel > rBarPixel)
+				{
+					val++;
+				}
+				bars[val][y - 3].second.append(x);
+			}
+		}
+		foreach(quint32 bidx, bars.keys())
+		{
+			foreach(quint32 y, bars[bidx].keys())
+			{
+				QPair<QList<quint32>, QList<quint32> > pts = bars[bidx][y];
+				if(pts.first.isEmpty() || pts.second.isEmpty())
+				{
+					continue;
+				}
+				// math the left edge of this bar
+				CVector lScreenPos(pts.first.first(), 959.0 - y, 1.0);
+				CVector rScreenPos(pts.second.first(), 962.0 - y, 1.0);
+				CVector lRealPos = Matrix::UnprojectPoint(lScreenPos, matLMv, matPrj, viewport);
+				CVector rRealPos = Matrix::UnprojectPoint(rScreenPos, matRMv, matPrj, viewport);
+				CVector b1, b2;
+				CVector::GetShortestBridge(leftCameraPos, lRealPos, rightCameraPos, rRealPos, b1, b2);
+				if((b1 - b2).Length() < 10.0)
+				{
+					CVector realPos = (b1 + b2) / 2.0;
+					PointCC pxc;
+					pxc.realDot = realPos;
+					pxc.leftDot = QPoint(pts.first.first(), y);
+					pxc.rightDot = QPoint(pts.first.first(), y - 3);
+					pxc.color[0] = cvGet2D(colorImg, y, pts.first.first()).val[2];
+					pxc.color[1] = cvGet2D(colorImg, y, pts.first.first()).val[1];
+					pxc.color[2] = cvGet2D(colorImg, y, pts.first.first()).val[0];
+					finalPoints.append(pxc);
+				}
+				// then math the right edge
+				if(pts.first.count() == 1 && pts.second.count() == 1)
+				{
+					continue;
+				}
+				lScreenPos = CVector(pts.first.last(), 959.0 - y, 1.0);
+				rScreenPos = CVector(pts.second.last(), 962.0 - y, 1.0);
+				lRealPos = Matrix::UnprojectPoint(lScreenPos, matLMv, matPrj, viewport);
+				rRealPos = Matrix::UnprojectPoint(rScreenPos, matRMv, matPrj, viewport);
+				CVector::GetShortestBridge(leftCameraPos, lRealPos, rightCameraPos, rRealPos, b1, b2);
+				if((b1 - b2).Length() < 10.0)
+				{
+					CVector realPos = (b1 + b2) / 2.0;
+					PointCC pxc;
+					pxc.realDot = realPos;
+					pxc.leftDot = QPoint(pts.first.last(), y);
+					pxc.rightDot = QPoint(pts.first.last(), y - 3);
+
+					pxc.color[0] = cvGet2D(colorImg, y, pts.first.last()).val[2];
+					pxc.color[1] = cvGet2D(colorImg, y, pts.first.last()).val[1];
+					pxc.color[2] = cvGet2D(colorImg, y, pts.first.last()).val[0];
+					finalPoints.append(pxc);
+				}
+			}
+		}
+
+
+		//
+
+
+		//cvReleaseImage(&lCalBin);
+		//cvReleaseImage(&rCalBin);
+	}
+
+	Save("x.ds3d");
+
+
+
+
+
+
+	//int cannyThreshold = 10;
+	//for(int i = 0; i < 12; i++)
+	//{
+	//	cvCanny(lBars[i], lCanny[i], cannyThreshold, cannyThreshold * 2, 3);
+	//	cvCanny(rBars[i], rCanny[i], cannyThreshold, cannyThreshold * 2, 3);
+	//	cvCanny(lRBars[i], lRCanny[i], cannyThreshold, cannyThreshold * 2, 3);
+	//	cvCanny(rRBars[i], rRCanny[i], cannyThreshold, cannyThreshold * 2, 3);
+	//	cvAnd(lCanny[i], lRCanny[i], lAndCanny[i]);
+	//	cvAnd(rCanny[i], rRCanny[i], rAndCanny[i]);
+	//}
 
 	//int threshold = 20;
 	//CvSize lSize = cvGetSize(lAll);
@@ -142,7 +373,16 @@ int main(int argc, char *argv[])
 	//}
 	//qDebug() << etimer.elapsed();
 
-	cvShowImage("ImageView", rRBars[7]);
+	//cvShowImage("ImageView", lBinMap);
+
+
+	//for(int i = 0; i < 1280; i++)
+	//{
+	//	unsigned short *pData = (unsigned short*)(lBinMap->imageData);
+	//	pData += 1280 * 520 + i;
+	//	qDebug() << *(pData);
+	//}
+
 
 	for(int i = 0; i < 12; i++)
 	{
@@ -161,5 +401,7 @@ int main(int argc, char *argv[])
 	cvReleaseImage(&lDark);
 	cvReleaseImage(&rAll);
 	cvReleaseImage(&rDark);
+	cvReleaseImage(&lBinMap);
+	cvReleaseImage(&rBinMap);
 	return a.exec();
 }
